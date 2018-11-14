b5c3b4c7fb592c505fadc93fd704763b07c8cfdd
Simon Balean <simonbalean@gmail.com>
2018-11-14 11:17:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,7 @@ def oxford_comma(array)
     elsif array_size == 2 
       return "#{array[0]} and #{array[1]}"
     elsif array_size > 2 
+      return 
     array.split(0..-1).join(', ') + ', and ' + array.split(-1);
   end
   return array
