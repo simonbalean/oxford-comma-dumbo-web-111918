e63157e24c35be773a11d153a1d721b772540350
Simon Balean <simonbalean@gmail.com>
2018-11-14 11:20:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,11 @@ def oxford_comma(array)
   
   if array_size == 1 
     return "#{array.first}"
-    elsif array_size == 2 
-      return "#{array[0]} and #{array[1]}"
-    elsif array_size > 2 
-      return 
+  elsif array_size == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif array_size > 2 
+    return 
 #    array.split(0..-1).join(', ') + ', and ' + array.split(-1);
-    end
-    array.join(", ")
-  return array
-#  array.join(", ")
+  end
+  array.join(", ")
 end
